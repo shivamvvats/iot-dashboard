2eb94edb89d75e07197a1a75bcf412a9b4e8792d
Shivam Vats <shivamvats8284@gmail.com>
2026-05-04 21:45:48 +0530

    updated UI

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function updateData() {
   let temp = Math.floor(Math.random() * 40);
+  let humidity = Math.floor(Math.random() * 100);
+
   document.getElementById("temp").innerText = temp + " °C";
+  document.getElementById("humidity").innerText = humidity + " %";
 
   if (temp > 30) {
     document.getElementById("fire").innerText = "🔥 Fire Risk!";
